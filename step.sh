10b1a03549af4ca52120dd1bcee59fed0dcdeee2
Ashwin Nair <ashwin.nair.ext@proximus.com>
2021-10-25 16:03:18 +0200

    Fix step when generating report

--- step.sh
+++ step.sh
@@ -14,9 +14,16 @@ RESULTS=$($THIS_SCRIPT_DIR/checkCircularDependency.js "${dir_path}" "${ts_config
 TOTAL_CIRCULAR_DEPENDENCIES_COUNT=0
 for i in ${RESULTS//,/ }
 do
+  if [ ! -f "list_circular_dependencies.txt" ]; then
+    printf "Circular Dependencies\n\n\n" > list_circular_dependencies.txt
+  fi
   echo "$i" >> list_circular_dependencies.txt
   TOTAL_CIRCULAR_DEPENDENCIES_COUNT=`expr $TOTAL_CIRCULAR_DEPENDENCIES_COUNT + 1`
 done
+
+if [ -f "list_circular_dependencies.txt" ]; then
+    cp list_circular_dependencies.txt $BITRISE_DEPLOY_DIR/list_circular_dependencies.txt
+fi
 
 
 echo "---- REPORT ----"
@@ -31,6 +38,8 @@ printf "TS Config file path: $ts_config_path \n" >> quality_report.txt
 printf "Total Circular dependencies count threshold: $circular_dep_count_threshold \n" >> quality_report.txt
 printf "Total Circular dependencies detected: $TOTAL_CIRCULAR_DEPENDENCIES_COUNT \n" >> quality_report.txt
 printf "You can see list of circular dependencies in list_circular_dependencies.txt \n\n" >> quality_report.txt
+
+cp quality_report.txt $BITRISE_DEPLOY_DIR/quality_report.txt || true
 
 envman add --key TOTAL_CIRCULAR_DEPENDENCIES_COUNT --value $TOTAL_CIRCULAR_DEPENDENCIES_COUNT
 
